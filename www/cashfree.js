b6f097c02c76cfb9bffed83be8bae9233f6ec239
Raj Nandan Sharma <raj@devo.gocashfree.com>
2020-06-30 15:03:34 +0530

    handle inaappborwser

--- www/cashfree.js
+++ www/cashfree.js
@@ -10,16 +10,21 @@ var Cashfree = {
         return new Promise(function(resolve, reject) {
             var order = sdkInput.order;
             var url = "https://test.cashfree.com/billpay/checkout/post/submit";
+            var toolbarcolor = "#7d49f0";
+            var closebuttoncolor = "#ffffff";
+
+            if (sdkInput.toolbarcolor) toolbarcolor = sdkInput.toolbarcolor;
+            if (sdkInput.closebuttoncolor) closebuttoncolor = sdkInput.closebuttoncolor;
             if (sdkInput.env !== undefined && sdkInput.env == "PROD") url = "https://www.cashfree.com/checkout/post/submit";
+           
             var pageContent = '<html><body><form id="redirectForm" method="post" action="' + url + '">';
             pageContent = pageContent + '<input type="hidden" name="source" value="cordova-sdk"/>';
             pageContent = pageContent + '<input type="hidden" name="appId" value="' + sdkInput.appId + '"/>';
             pageContent = pageContent + '<input type="hidden" name="tokenData" value="' + sdkInput.cftoken + '"/>';
-            pageContent = pageContent + '<input type="hidden" name="returnUrl" value="https://92e12e4ca49e.ngrok.io/play/return2.php"/>';
+            //pageContent = pageContent + '<input type="hidden" name="returnUrl" value="https://92e12e4ca49e.ngrok.io/play/return2.php"/>';
             for (let key in order) {
                 if (order.hasOwnProperty(key)) {
                     pageContent = pageContent + '<input type="hidden" name="' + key + '" value="' + order[key] + '"/>';
-
                 }
             }
             pageContent = pageContent + '</form><script type="text/javascript">window.onload = function(){document.getElementById("redirectForm").submit()};</script></body></html>';
@@ -31,12 +36,17 @@ var Cashfree = {
                 "hideurlbar=yes",
                 "clearsessioncache=yes",
                 "clearcache=yes",
-                "toolbarcolor=#7d49f0",
+                "toolbarcolor=" + toolbarcolor,
                 "hardwareback=no",
-                "closebuttoncolor=#ffffff",
+                "closebuttoncolor=" + closebuttoncolor,
                 "useWideViewPort=no",
                 "lefttoright=yes",
-                "zoom=no"
+                "zoom=no",
+                /**ios*/
+                "hidespinner=no",
+                "toolbarposition=top",
+                "enableViewportScale=no",
+                "enableViewportScale=yes"
             ];
 
             var browserRef = cordova.InAppBrowser.open(
